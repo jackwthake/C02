27edfcaccfbad57e6adb24edd739c3478c87be3e
jackwthake <jackthake@hotmail.com>
2026-05-18 20:41:16 -0700

    Can now read from registers

--- src/generator.rs
+++ src/generator.rs
@@ -1,11 +1,15 @@
 use crate::parser::{TopLevel, Expr, Stmt, Op, Type};
 use crate::analyzer::{SymbolTable, Symbol};
 
+use std::collections::HashMap;
+
 struct Generator {
   output: String,
   reg_depth: usize,
-  label_count: usize,  // for unique if/while jump labels
+  label_count: usize,
   symbol_table: SymbolTable,
+  local_offsets: HashMap<String, u8>,  // name -> FP offset
+  current_frame_size: u8,              // grows as we encounter VarDecls
 }
 
 impl Generator {
@@ -15,12 +19,63 @@ impl Generator {
       reg_depth: 0,
       label_count: 0,
       symbol_table,
+      local_offsets: HashMap::new(),
+      current_frame_size: 0,
     }
   }
   
   fn emit(&mut self, line: &str) {
     self.output.push_str(line);
     self.output.push('\n');
+  }
+  
+  fn emit_local_store(&mut self, name: &str, data_type: &Type) {
+    let offset = *self.local_offsets.get(name)
+    .expect("local_store called for undeclared local");
+    
+    match data_type {
+      Type::U8 | Type::I8 => {
+        self.emit(&format!("  LDY #${:02X}", offset));
+        self.emit("  LDA r0");
+        self.emit("  STA (FP),Y");
+      }
+      Type::U16 | Type::I16 => {
+        // low byte
+        self.emit(&format!("  LDY #${:02X}", offset));
+        self.emit("  LDA r0");
+        self.emit("  STA (FP),Y");
+        // high byte
+        self.emit(&format!("  LDY #${:02X}", offset + 1));
+        self.emit("  LDA r0+1");
+        self.emit("  STA (FP),Y");
+      }
+      _ => unreachable!()
+    }
+  }
+  
+  fn emit_local_load(&mut self, name: &str, data_type: &Type) {
+    let offset = *self.local_offsets.get(name)
+    .expect("local_load called for undeclared local");
+    let reg = format!("r{}", self.reg_depth);
+    
+    match data_type {
+      Type::U8 | Type::I8 => {
+        self.emit(&format!("  LDY #${:02X}", offset));
+        self.emit("  LDA (FP),Y");
+        self.emit(&format!("  STA {}", reg));
+        self.emit(&format!("  LDA #$00"));
+        self.emit(&format!("  STA {}+1", reg));
+      }
+      Type::U16 | Type::I16 => {
+        self.emit(&format!("  LDY #${:02X}", offset));
+        self.emit("  LDA (FP),Y");
+        self.emit(&format!("  STA {}", reg));
+        self.emit(&format!("  LDY #${:02X}", offset + 1));
+        self.emit("  LDA (FP),Y");
+        self.emit(&format!("  STA {}+1", reg));
+      }
+      _ => unreachable!()
+    }
   }
   
   fn fresh_label(&mut self) -> String {
@@ -55,19 +110,16 @@ impl Generator {
   fn gen_stmt(&mut self, stmt: &Stmt) {
     match stmt {
       Stmt::Assign(name, expr) => {
-        // evaluate expr into r0
         self.reg_depth = 0;
         self.gen_expr(expr);
-
-        // look up what name is in symbol table
-        match self.symbol_table.local_symbols.get(name) {
-          Some(Symbol::Register { address, .. }) => {
-            let address = *address;
-            self.emit(&format!("  LDA r0"));
-            self.emit(&format!("  STA ${:04X}", address));
-          }
-          _ => todo!("non-register assign")
-        }
+        
+        let address = match self.symbol_table.local_symbols.get(name) {
+          Some(Symbol::Register { address, .. }) => *address,
+          _ => todo!("non-register assign"),
+        };
+        
+        self.emit("  LDA r0");
+        self.emit(&format!("  STA ${:04X}", address));
       }
       Stmt::Return(None) => {
         self.emit("  RTS");
@@ -77,6 +129,25 @@ impl Generator {
         self.gen_expr(expr);
         // return value convention — leave in r0
         self.emit("  RTS");
+      }
+      Stmt::VarDecl(data_type, name, initializer) => {
+        // allocate space on software stack
+        let offset = self.current_frame_size;
+        self.local_offsets.insert(name.clone(), offset);
+        
+        // grow frame by type width
+        match data_type {
+          Type::U8 | Type::I8 => self.current_frame_size += 1,
+          Type::U16 | Type::I16 => self.current_frame_size += 2,
+          _ => unreachable!()
+        }
+        
+        // if there's an initializer evaluate and store it
+        if let Some(expr) = initializer {
+          self.reg_depth = 0;
+          self.gen_expr(expr);
+          self.emit_local_store(name, data_type);
+        }
       }
       _ => todo!("stmt")
     }
@@ -93,17 +164,44 @@ impl Generator {
       }
       Expr::Identifier(name) => {
         let reg = format!("r{}", self.reg_depth);
-        match self.symbol_table.local_symbols.get(name) {
-          Some(Symbol::Register { address, .. }) => {
+        let symbol = self.symbol_table.local_symbols.get(name);
+
+        match symbol {
+          Some(Symbol::Register { address, data_type }) => {
             let address = *address;
-            self.emit(&format!("  LDA ${:04X}", address));
-            self.emit(&format!("  STA {}", reg));
-            self.emit(&format!("  LDA #$00"));
-            self.emit(&format!("  STA {}+1", reg));
+            let data_type = data_type.clone();
+            let _ = symbol;
+            match data_type {
+              Type::U8 | Type::I8 => {
+                self.emit(&format!("  LDA ${:04X}", address));
+                self.emit(&format!("  STA {}", reg));
+                self.emit("  LDA #$00");
+                self.emit(&format!("  STA {}+1", reg));
+              }
+              Type::U16 | Type::I16 => {
+                self.emit(&format!("  LDA ${:04X}", address));
+                self.emit(&format!("  STA {}", reg));
+                self.emit(&format!("  LDA ${:04X}", address + 1));
+                self.emit(&format!("  STA {}+1", reg));
+              }
+              _ => unreachable!("registers cannot be void"),
+            }
           }
-          _ => todo!("identifier expr")
+          Some(Symbol::Variable { data_type }) => {
+            let data_type = data_type.clone();
+            let is_local = self.local_offsets.contains_key(name);
+            let _ = symbol;
+            if is_local {
+              self.emit_local_load(name, &data_type);
+            } else {
+              todo!("global variable load")
+            }
+          }
+          Some(Symbol::Function { .. }) => unreachable!("function used as expression"),
+          None => unreachable!("undefined identifier"),
         }
       }
+      
       _ => todo!("expr")
     }
   }
